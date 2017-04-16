bd5dda0057ca4c79b25e863356bb886e525b329f
Doomoocloo <Doomoocloo@gmail.com>
2017-04-16 14:59:47 -0400

    More of C# implementation

--- College Event Planner/ASP files/COP 4710 College App/Controllers/HomeController.cs
+++ College Event Planner/ASP files/COP 4710 College App/Controllers/HomeController.cs
@@ -12,9 +12,10 @@ namespace COP_4710_College_App.Controllers
         [HttpGet]
         public ActionResult LoginPage()
         {
+            Session["loggedIn"] = true;
+            Session["privilegeID"] = 2;
 
-
-            if(Models.SessionHandler.loggedIn())
+            if (Models.SessionHandler.loggedIn())
                 return RedirectToAction("HomePage");
             else
             {
@@ -43,6 +44,8 @@ namespace COP_4710_College_App.Controllers
                 return RedirectToAction("LoginPage");
             }
 
+            ViewBag.schools = Models.SchoolData.viewSchools();
+            Session["numSchools"] = ViewBag.schools.Count;
             //get number of schools
             //get number of events
             //get number of modified events?
